b72b2ed6e8365a3795da10fcf85b1be71ea495bc
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-10-09 16:06:21 +0200

    create data dir if not exists

--- src/utils/incentives.ts
+++ src/utils/incentives.ts
@@ -3,8 +3,13 @@ import fs from 'fs';
 import { safeParse, safeStringify } from './parse';
 import { IncentiveExtended } from '../interfaces/IncentiveExtended';
 
+const getIncentivesDir = (): string => {
+    return path.resolve(__dirname, "../../data");
+}
+
 const getIncentivesPath = (): string => {
-    return path.resolve(__dirname, "../../data/incentives.json");
+    const dir = getIncentivesDir();
+    return path.resolve(dir, `incentives.json`);
 }
 
 export const getIncentives = async () => {
@@ -17,6 +22,12 @@ export const getIncentives = async () => {
 }
 
 export const writeIncentives = (incentives: IncentiveExtended[]) => {
+    
+    const dir = getIncentivesDir();
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+
     const path = getIncentivesPath();
     fs.writeFileSync(path, safeStringify(incentives), { encoding: 'utf-8' });
     console.log(`💾 Incentives saved to ${path}`);
