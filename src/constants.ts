dec206390b0feedecee48e614ec726201056bb98
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-10-30 16:58:25 +0100

    feat: new merkle address

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 import { Address } from "viem";
 
-export const MERKL_CONTRACT = "0xE91Dd4c8bf5533D2b678eCf089eF0bcAa79C0CcD" as Address;
+export const MERKL_CONTRACT = "0x183e7d025eab332c799e8122c5b163a63b95423c" as Address;
