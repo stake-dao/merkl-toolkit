ea73b5d965688da13d6e4fb7ee8f0db58a1bc7f2
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-11-03 10:55:48 +0100

    fix: new merkle address

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 import { Address } from "viem";
 
-export const MERKL_CONTRACT = "0x183e7d025eab332c799e8122c5b163a63b95423c" as Address;
+export const MERKL_CONTRACT = "0xd4898a378ea555595c4e7dbde722b134a3f346d1" as Address;
